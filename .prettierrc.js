7bfc8d3a17eb490b41e4d83a910bccede0cd6ecb
Mandy Tsai <mandy3152618@gmail.com>
2022-10-13 11:02:58 +0800

    build: Prettier ESLint 套件環境

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "semi": true,
+  "trailingComma": "none",
+  "tabWidth": 2,
+  "printWidth": 120,
+  "singleQuote": true,
+  "bracketSpacing": true,
+  "arrowParens": "avoid"
+}
--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
+   <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="font-semibold text-6xl text-blue-600">2022 iThome</h1>
+      <p class="font-bold mt-4 text-9xl text-gray-900">鐵人賽</p>
+    </div>
   </div>
 </template>
