4d7a331ce4d9705ac65f5049e0580b07ffcc94df
Mandy Tsai <mandy3152618@gmail.com>
2022-10-13 11:40:47 +0800

    add: 首頁

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <header class="bg-red-200 w-full p-2">header</header>
+    <slot />
+    <footer class="bg-red-200 w-full p-2">footer</footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="font-semibold p-2 text-6xl text-blue-600">2022 Nuxt3 Practice</h1>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
