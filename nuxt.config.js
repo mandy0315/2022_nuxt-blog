669cf84168f9fc228b1c7348a9a84a821b8ae48b
Mandy Tsai <mandy3152618@gmail.com>
2022-10-18 15:05:07 +0800

    build: 設定 sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  '@': '/<rootDir>', // 別名
   modules: ['@nuxtjs/tailwindcss'],
-})
+  css: ['@/assets/style/base.scss']
+});
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,10 @@
     <div class="flex flex-col items-center">
       <h1 class="font-semibold p-2 text-6xl text-blue-600">2022 Nuxt3 Practice</h1>
     </div>
+    <a href="">test</a>
   </div>
 </template>
 
 <script setup></script>
 
-<style></style>
+<style lang="scss" scoped></style>
