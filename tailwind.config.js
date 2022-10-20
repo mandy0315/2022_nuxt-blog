885ec48d66b8d816193cc6a727407d138de84234
Mandy Tsai <mandy3152618@gmail.com>
2022-10-20 14:18:50 +0800

    style: 設定自訂顏色

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const customColors = {
+  // Configure your color palette here
+  'c-gray-800': '#323232',
+  'c-gray-600': '#575757',
+  'c-gray-400': '#D4D4D4',
+  'c-orange-800': '#E98012',
+  'c-orange-600': '#F7B168',
+  'c-orange-200': '#FDF8F2',
+  'c-brown-800': '#7A6A56'
+};
 module.exports = {
   content: [
     './components/**/*.{vue,js,ts}',
@@ -9,7 +19,20 @@ module.exports = {
     './app.{js,ts,vue}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      textColor: theme => ({
+        ...theme('colors'),
+        ...customColors
+      }),
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        ...customColors
+      }),
+      borderColor: theme => ({
+        ...theme('colors'),
+        ...customColors
+      })
+    }
   },
   plugins: [require('@tailwindcss/forms')]
 };
