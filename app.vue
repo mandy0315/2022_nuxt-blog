fa0017e26f3af4d8a0a7e09c6cf087c4dad1fd21
Mandy Tsai <mandy3152618@gmail.com>
2022-10-20 17:36:43 +0800

    add: 設定 head

--- app.vue
+++ app.vue
@@ -3,3 +3,30 @@
     <nuxt-page />
   </nuxt-layout>
 </template>
+
+<script setup>
+useHead({
+  meta: [
+    {
+      name: 'description',
+      content: '這是一個 Nuxt3 的練習 Blog'
+    },
+    {
+      name: 'author',
+      content: 'mandy'
+    },
+    {
+      name: 'copyright',
+      content: '2022.mandy'
+    }
+  ],
+  link: [
+    {
+      rel: 'shortcut icon',
+      href: 'images/icon/logo.ico'
+    }
+  ]
+});
+</script>
+
+<style lang="scss" scoped></style>
