6c8a273995d0423bce2f893cae44e8e826c41a8d
Mandy Tsai <mandy3152618@gmail.com>
2022-10-20 22:45:18 +0800

    style: 後台 header 樣式

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ const customColors = {
   'c-gray-800': '#323232',
   'c-gray-600': '#575757',
   'c-gray-400': '#D4D4D4',
-  'c-orange-800': '#E98012',
+  'c-orange-800': '#FF870A',
   'c-orange-600': '#F7B168',
   'c-orange-200': '#FDF8F2',
   'c-brown-800': '#7A6A56'
