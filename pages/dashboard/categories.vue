524ba14ab8be4db617c505fccf7157c99128d0bb
Mandy Tsai <mandy3152618@gmail.com>
2022-11-02 15:52:50 +0800

    add: 分類管理方法

--- pages/dashboard/categories.vue
+++ pages/dashboard/categories.vue
@@ -11,21 +11,31 @@
               <th scope="row" class="w-3/12 py-2 px-4"></th>
             </tr>
           </thead>
+
           <tbody>
-            <tr class="border-b border-solid border-c-gray-400 text-base">
-              <td scope="col" class="py-2 px-4 text-c-gray-600">
-                <p class="text-c-gray-800">Nuxt</p>
-              </td>
-              <td class="py-2 px-4">
-                <input
-                  type="text"
-                  value="/categories/nuxt"
-                  class="form-input w-full rounded border-c-gray-400 !bg-gray-100 py-1"
-                  disabled
-                />
-              </td>
-              <td class="py-2 px-4"><button class="c-border-button c-border-button-red my-1 w-full">刪除</button></td>
-            </tr>
+            <template v-if="categorieList.length > 0">
+              <tr v-for="categorie in categorieList" class="border-b border-solid border-c-gray-400 text-base">
+                <td scope="col" class="py-2 px-4 text-c-gray-600">
+                  <p class="text-c-gray-800">{{ categorie.name }}</p>
+                </td>
+                <td class="py-2 px-4">
+                  <input
+                    type="text"
+                    :value="categorie.path"
+                    class="form-input w-full rounded border-c-gray-400 !bg-gray-100 py-1"
+                    disabled
+                  />
+                </td>
+                <td class="py-2 px-4">
+                  <button
+                    class="c-border-button c-border-button-red my-1 w-full"
+                    @click.prevent="deleteCategorie(categorie.id)"
+                  >
+                    刪除
+                  </button>
+                </td>
+              </tr>
+            </template>
           </tbody>
         </table>
       </section>
@@ -33,16 +43,29 @@
         <h2 class="pb-4 text-xl font-medium">
           <Icon icon="ic:round-create-new-folder" class="mr-2 inline-block align-middle" />新增分類
         </h2>
+        <p v-if="isSamePath">已有相同路徑</p>
         <form>
           <label class="block pb-4">
             <p class="pb-1 pr-4 text-base">名稱</p>
-            <input type="text" class="form-input w-full rounded border-c-gray-400" placeholder="請輸入名稱" />
+            <input
+              v-model.lazy="fillCategorieName"
+              type="text"
+              class="form-input w-full rounded border-c-gray-400"
+              placeholder="請輸入名稱"
+            />
           </label>
           <label class="block pb-4">
             <p class="pb-1 pr-4 text-base">路徑</p>
-            <input type="text" class="form-input w-full rounded border-c-gray-400" placeholder="請輸入路徑" />
+            <input
+              v-model.lazy="fillCategoriePath"
+              type="text"
+              class="form-input w-full rounded border-c-gray-400"
+              placeholder="請輸入路徑"
+            />
           </label>
-          <button class="c-rounded-button c-rounded-button-gray ml-auto block rounded">新增</button>
+          <button @click.prevent="addCategorie" class="c-rounded-button c-rounded-button-gray ml-auto block rounded">
+            新增
+          </button>
         </form>
       </section>
     </div>
@@ -50,12 +73,54 @@
 </template>
 
 <script setup>
-useHead({
-  title: '分類管理'
-});
+import { useBlogStore } from '@/stores/index';
 definePageMeta({
   layout: 'dashboard'
 });
+
+const { addFirebaseData, deleteFirebaseData } = useFirebase();
+const $blogStore = useBlogStore();
+const categorieList = computed(() => $blogStore.categorieList);
+const isSamePath = ref(false);
+const categorieInfo = reactive({});
+const fillCategorieName = computed({
+  get: () => categorieInfo.name,
+  set: val => {
+    isSamePath.value = false;
+    categorieInfo.name = val;
+  }
+});
+const fillCategoriePath = computed({
+  get: () => categorieInfo.path,
+  set: val => {
+    isSamePath.value = false;
+    categorieInfo.path = val;
+  }
+});
+
+const init = async () => {
+  await $blogStore.updateCategorieList();
+};
+init();
+
+const addCategorie = async () => {
+  isSamePath.value = categorieList.value.filter(item => item.path === categorieInfo.path).length > 0;
+  if (isSamePath.value) return false;
+
+  const data = await addFirebaseData('categories', categorieInfo);
+  if (data.success) {
+    await $blogStore.updateCategorieList();
+  }
+  fillCategorieName.value = '';
+  fillCategoriePath.value = '';
+};
+
+const deleteCategorie = async id => {
+  const data = await deleteFirebaseData('categories', id);
+  if (data.success) {
+    await $blogStore.updateCategorieList();
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
