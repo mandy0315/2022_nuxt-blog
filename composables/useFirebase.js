99e84b36b559798cc12106576f905a30631581fb
Mandy Tsai <mandy3152618@gmail.com>
2022-11-03 15:46:19 +0800

    delete: 刪除共用api

--- composables/useFirebase.js
+++ composables/useFirebase.js
@@ -50,42 +50,6 @@ export default function () {
     return data.value;
   };
 
-  // const getFirebaseDocData = async (col, id) => {
-  //   const { data } = await useFetch(`/api/firebase/docData?col=${col}&id=${id}`, {
-  //     initialCache: false
-  //   });
-  //   return data.value;
-  // };
-  // const getFirebaseData = async col => {
-  //   const { data } = await useFetch(`/api/firebase/data?col=${col}`, {
-  //     initialCache: false
-  //   });
-  //   return data.value;
-  // };
-  // const addFirebaseData = async (col, doc) => {
-  //   const { data } = await useFetch(`/api/firebase/add?col=${col}`, {
-  //     method: 'post',
-  //     body: doc,
-  //     initialCache: false
-  //   });
-  //   return data.value;
-  // };
-  // const updateFirebaseData = async (col, id, doc) => {
-  //   const { data } = await useFetch(`/api/firebase/update?col=${col}&id=${id}`, {
-  //     method: 'post',
-  //     body: doc,
-  //     initialCache: false
-  //   });
-  //   return data.value;
-  // };
-  // const deleteFirebaseData = async (col, id) => {
-  //   const { data } = await useFetch(`/api/firebase/delete?col=${col}&id=${id}`, {
-  //     method: 'delete',
-  //     initialCache: false
-  //   });
-  //   return data.value;
-  // };
-
   return {
     getCategoriesListAPI,
     addCategoriesAPI,
