2b164367fd1aeb29d3390635f2b990809e1221a6
Mandy Tsai <mandy3152618@gmail.com>
2022-11-08 22:04:22 +0800

    wip: 側邊欄待續

--- components/ui/post/sidebar.vue
+++ components/ui/post/sidebar.vue
@@ -3,17 +3,7 @@
     class="ml-6 w-full max-w-[300px] rounded-md bg-white p-6 text-c-gray-800 shadow-md dark:bg-c-gray-600 dark:text-c-gray-400"
   >
     <div>
-      <p class="text-lg font-medium">
-        <Icon icon="material-symbols:folder-open-rounded" class="mr-2 inline-block" />文章分類
-      </p>
-      <ul class="mt-2 px-2">
-        <li>
-          <nuxt-link v-for="num in 2" class="flex h-[30px]"
-            ><span class="c-text-button inline-block rounded-l bg-gray-100 px-2">Nuxt</span
-            ><span class="inline-block rounded-r bg-c-gray-400 pl-1.5 pr-2 text-sm">1</span></nuxt-link
-          >
-        </li>
-      </ul>
+      <p class="text-lg font-medium">待續</p>
     </div>
   </aside>
 </template>
