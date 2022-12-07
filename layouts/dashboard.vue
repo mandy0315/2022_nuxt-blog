6d06a8bbe1be3d37bf630c8d366233d9267ad26f
Mandy Tsai <mandy3152618@gmail.com>
2022-12-08 00:09:17 +0800

    style: 新增樣式 & add: 新增 topNav 元件

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="min-h-screen bg-gray-100">
+    <top-nav />
     <header-dashboard />
     <slot />
     <footer-default />
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-screen bg-gray-50 text-c-gray-800 dark:bg-c-gray-800 dark:text-c-gray-400">
+  <div class="min-h-screen bg-gray-100">
+    <top-nav />
     <header-default />
     <main class="c-container px-4 pt-6 pb-20 lg:px-0">
       <slot />
