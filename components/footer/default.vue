a63b0fa5cc6b94bbc8dc990cc899127878006a8c
Mandy Tsai <mandy3152618@gmail.com>
2022-12-08 14:47:18 +0800

    fix: footer 樣式

--- components/footer/default.vue
+++ components/footer/default.vue
@@ -1,5 +1,13 @@
 <template>
-  <footer class="border-soild border-t border-c-gray-400 py-2 text-center text-c-gray-800">@2022.mandy</footer>
+  <footer class="border-soild border-t border-c-gray-400">
+    <div class="c-container flex justify-between py-5">
+      <p>@2022.mandy_生活&技術筆記</p>
+      <div>
+        <a href="#" class="mx-1"><Icon icon="mingcute:github-fill" class="inline-block text-lg" /></a>
+        <a href="#" class="mx-1"><Icon icon="ri:facebook-fill" class="inline-block text-lg" /></a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup></script>
