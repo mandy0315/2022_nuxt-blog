6c6fcf67f3ec0c7ae7a13c8a4d51e0101aedbeea
Mandy Tsai <mandy3152618@gmail.com>
2022-12-20 11:06:50 +0800

    doc: 判斷後台條件的註解

--- middleware/main.global.js
+++ middleware/main.global.js
@@ -1,7 +1,7 @@
 import { useMainStore } from '@/stores/index';
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  // 是否是後台頁面
   const $mainStore = useMainStore();
-  $mainStore.isDashboardPages = /^\/dashboard/g.test(to.path);
+
+  $mainStore.isDashboardPages = /^\/dashboard/g.test(to.path); // 是否是後台頁面
 });
--- stores/mainStore.js
+++ stores/mainStore.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export default defineStore('mainStore', {
   state: () => ({
     webTitle: 'Mandy 生活&技術筆記',
-    isDashboardPages: false
+    isDashboardPages: false // middleware - main.global 判定目前 path
   }),
   getters: {},
   actions: {}
