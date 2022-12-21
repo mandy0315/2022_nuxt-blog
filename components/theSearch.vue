b3e0ebd743f1c3c24aa180925e3f7d8b79375823
Mandy Tsai <mandy3152618@gmail.com>
2022-12-21 16:57:55 +0800

    add: search元件樣式

---
+++ components/theSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex">
+    <input
+      type="search"
+      placeholder="搜尋文章標題、摘要"
+      autocomplete="off"
+      class="grow rounded-l border-t border-b border-l border-solid border-c-gray-200 py-1 pl-2 text-c-gray-400"
+    />
+    <button class="c-rounded-button c-rounded-button-brown w-18 rounded-r rounded-l-none">搜尋</button>
+  </div>
+</template>
+
+<script setup></script>
