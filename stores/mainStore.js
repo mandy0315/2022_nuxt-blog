cd258534d554db93b93649402495391ae14b5c07
Mandy Tsai <mandy3152618@gmail.com>
2023-01-04 10:17:04 +0800

    add: els 狀態

--- stores/mainStore.js
+++ stores/mainStore.js
@@ -3,7 +3,8 @@ import { defineStore } from 'pinia';
 export default defineStore('mainStore', {
   state: () => ({
     webTitle: 'Mandy 生活&技術筆記',
-    isDashboardPages: false // middleware - main.global 判定目前 path
+    isDashboardPages: false, // middleware - main.global 判定目前 path
+    els: {}
   }),
   getters: {},
   actions: {}
