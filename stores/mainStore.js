4e2fce572ddd162a3ed3b7c779c94c88f0e27b68
Mandy Tsai <mandy3152618@gmail.com>
2023-01-05 17:25:11 +0800

    delete: webTitle 欄位

--- stores/mainStore.js
+++ stores/mainStore.js
@@ -2,7 +2,6 @@ import { defineStore } from 'pinia';
 
 export default defineStore('mainStore', {
   state: () => ({
-    webTitle: "Mandy's 筆記",
     isDashboardPages: false, // middleware - main.global 判定目前 path
     els: {}
   }),
