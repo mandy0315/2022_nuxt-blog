aa24f34291c74956c7fd9f6c6a94989ba07fcce6
Mandy Tsai <mandy3152618@gmail.com>
2023-01-11 18:34:35 +0800

    wip: 登入方法

--- pages/dashboard/login.vue
+++ pages/dashboard/login.vue
@@ -1,6 +1,10 @@
 <template>
-  <form class="mx-auto max-w-[320px] pt-4">
+  <div class="mx-auto max-w-[320px] pt-4">
     <h1 class="text-2xl font-bold text-c-brown-600">Hello ~ 歡迎登入</h1>
+    <div v-if="isLoginfailed" class="bg-c-yellow-200/40 px-4 py-2">
+      <h2 class="text-lg">登入失敗！</h2>
+      <p>請檢查帳號與密碼是否有錯誤</p>
+    </div>
     <div>
       <form-fill-input v-model:value="accountFill" title="帳號" placeholder="請輸入信箱" />
       <p v-if="errors['account']" class="pt-1 text-sm text-red-600">{{ errors['account'] }}</p>
@@ -10,18 +14,25 @@
       <p v-if="errors['password']" class="pt-1 text-sm text-red-600">{{ errors['password'] }}</p>
     </div>
 
-    <button @click.prevent="handleSubmit" class="c-rounded-button c-rounded-button-brown my-4 w-full rounded-sm">
+    <div @click="handleSignIn" class="c-rounded-button c-rounded-button-brown my-4 w-full rounded-sm text-center">
       登入
-    </button>
-  </form>
+    </div>
+
+    <!-- <button @click.prevent="handleResetPassword">忘記密碼</button> -->
+  </div>
 </template>
 
 <script setup>
+import { useMainStore } from '@/stores/index';
+import { computed } from 'vue';
 useHead({ title: '會員登入' });
 definePageMeta({
   layout: false
 });
 
+const $mainStore = useMainStore();
+
+// 驗證
 const { values, errors, checkError, checkAllError } = useForm({
   initValues: {
     account: '',
@@ -33,12 +44,13 @@ const { values, errors, checkError, checkAllError } = useForm({
       { validate: val => /[\w._-]+@[a-z0-9.-]+\.[a-z]{2,4}/.test(val), errorMsg: '您輸入的信箱格式有誤' }
     ],
     password: [
-      { required: true, errorMsg: '必填請輸入信箱' },
+      { required: true, errorMsg: '必填請輸入密碼' },
       { validate: val => val.length >= 8, errorMsg: '您輸入的密碼至少含 8 個字' }
     ]
   }
 });
 
+// 欄位
 const accountFill = computed({
   get: () => values.account,
   set: val => {
@@ -54,9 +66,45 @@ const passwordFill = computed({
   }
 });
 
-const handleSubmit = () => {
+const getUidBySignIn = async () => {
+  const result = {
+    uid: '',
+    error: ''
+  };
+  const info = { ...values };
+  const { data, error } = await useFetch('/api/firebase/member/signIn', {
+    method: 'post',
+    body: info,
+    initialCache: false
+  });
+  if (data.value) {
+    result.uid = data.value.data?.memberId;
+  } else {
+    result.error = error.value.data;
+  }
+  return result;
+};
+
+const isLoginfailed = useState('isLoginfailed', () => false);
+// TODO handleSignIn未完
+const handleSignIn = async () => {
+  // 驗證錯誤
   const { isError } = checkAllError();
   if (isError) return false;
-  console.log('yes');
+  // 登入取得 uid
+  const { uid, error } = await getUidBySignIn();
+  if (!uid) {
+    isLoginfailed.value = error.statusCode === 400;
+    return false;
+  }
+  // // 更新會員資訊
+  // $mainStore.updateMemberInfo('uid', uid);
 };
+
+// const handleResetPassword = async () => {
+//   const { data } = await useFetch('/api/firebase/test', {
+//     method: 'post',
+//     initialCache: false
+//   });
+// };
 </script>
--- stores/mainStore.js
+++ stores/mainStore.js
@@ -1,10 +1,22 @@
 import { defineStore } from 'pinia';
-
+const defaultMemberInfo = {
+  email: null,
+  name: null,
+  uid: null
+};
 export default defineStore('mainStore', {
   state: () => ({
     isDashboardPages: false, // middleware - main.global 判定目前 path
-    els: {}
+    els: {},
+    memberInfo: { ...defaultMemberInfo }
   }),
-  getters: {},
-  actions: {}
+  getters: {
+    isLogin: state => state.memberInfo.uid !== null
+  },
+  actions: {
+    updateMemberInfo(item, val) {
+      const $store = this;
+      $store.memberInfo[item] = val;
+    }
+  }
 });
