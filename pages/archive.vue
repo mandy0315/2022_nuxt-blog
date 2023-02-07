b79142af52366e9afe39ae847f20ac60d814542e
Mandy Tsai <mandy3152618@gmail.com>
2023-02-07 18:52:07 +0800

    fix: /api/archive/list 檔案列表 api

--- pages/archive.vue
+++ pages/archive.vue
@@ -1,26 +1,19 @@
 <template>
-  <div class="c-container flex">
-    <div class="mr-4 grow">
+  <div class="c-container">
+    <div class="mr-4">
       <h1 class="c-title pb-3">檔案</h1>
-      <ul v-for="year in postsByYear" :key="year">
-        <h2 class="py-3 text-2xl font-bold text-c-brown-600">{{ year }}</h2>
-        <li v-for="post in postList" :key="post.id">
-          <nuxt-link v-if="post.year.includes(year)" :to="`/post/${post.id}`">
-            <div class="group block cursor-pointer py-1 px-2 text-xl hover:bg-c-gray-100">
-              <span class="pr-2 text-sm text-c-gray-400">{{ post.date }}</span
-              ><span class="group-hover:text-c-brown-600">{{ post.title }}</span>
-            </div>
-          </nuxt-link>
-        </li>
-      </ul>
+      <div v-if="postListByArchive.length > 0">
+        <h2>{{ postListByArchive[0].year }}年的筆記</h2>
+        <ul>
+          <li v-for="i in postListByArchive[0].list" :key="i.id">
+            <nuxt-link :to="`/post/${i.id}`">
+              <span>{{ i.update_at_date }}</span>
+              <span>{{ i.title }}</span>
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
     </div>
-    <!-- <div class="w-72">
-      <ul v-if="tagList.length > 0">
-        <li v-for="tag in tagList" :key="tag.name" class="inline-block">
-          <theTag :name="tag.name" :number="tag.count" :isActive="currentTag === tag.name" />
-        </li>
-      </ul>
-    </div> -->
   </div>
 </template>
 
@@ -28,60 +21,34 @@
 useHead({
   title: '檔案'
 });
-const tagList = useState('tagList', () => []);
-// const postList = useState('postList', () => []);
+const postListByArchive = useState('postListByArchive', () => []);
+const yearsByArchive = useState('yearsByArchive', () => []);
 
 const $route = useRoute();
-const currentTag = computed(() => $route.query.tag || 'all');
 
-const postsByYear = computed(() => {
-  const years = [];
-  postList.value.forEach(post => {
-    years.push(post.year);
-  });
-  return [...new Set(years)];
-});
-
-const getTagList = async () => {
-  const { data } = await useFetch('/api/firebase/archive/tagList', {
+const getArchiveList = async queryStr => {
+  const { data } = await useFetch('/api/firebase/archive/list', {
     method: 'get',
+    params: {
+      year: '2023'
+    },
     initialCache: false
   });
-  tagList.value = data.value.success && data.value.result.data;
+  if (data.value?.status === 'success') {
+    postListByArchive.value = data.value.data?.postList;
+    yearsByArchive.value = data.value.data?.years;
+  } else {
+    console.log(error.value?.data);
+    // showFailToast(error.value?.data?.statusMessage);
+  }
 };
-
-const { dateFormat } = useDateTime();
-// const getPostsList = async queryStr => {
-//   const queryByTag = queryStr.tag || '';
-
-//   const { data } = await useFetch('/api/firebase/archive/postList', {
-//     method: 'get',
-//     params: {
-//       tag: queryByTag
-//     },
-//     initialCache: false
-//   });
-
-//   if (data.value.success) {
-//     let posts = data.value.result?.data;
-
-//     posts.forEach(post => {
-//       const year = dateFormat(post.update_time, 'YYYY');
-//       const date = dateFormat(post.update_time, 'MM/DD');
-//       post.date = date; // 新增文章日期欄位
-//       post.year = year; // 新增文章年份欄位
-//     });
-//     postList.value = posts;
-//   }
-// };
 
 watch(
   () => $route.query,
   () => {
-    getPostsList($route.query);
+    getArchiveList($route.query);
   }
 );
 
-getPostsList($route.query);
-// getTagList();
+getArchiveList($route.query);
 </script>
---
+++ server/api/firebase/archive/list.get.js
@@ -0,0 +1,72 @@
+import useDateTime from '@/utils/useDateTime';
+import firebaseServer from '@/server/utils/useFirebaseServer';
+
+const getPostListFromFirebase = async () => {
+  const { db } = firebaseServer();
+  const { dateFormat } = useDateTime();
+  const postsRef = db.collection('posts');
+
+  const snapshot = await postsRef.where('status', '==', 'public').orderBy('update_time', 'desc').get();
+
+  const postList = [];
+  snapshot.forEach(doc => {
+    const post = doc.data();
+    const year = dateFormat(post.update_time, 'YYYY');
+    const date = dateFormat(post.update_time, 'MM-DD');
+    const obj = {
+      title: post.title,
+      id: post.id,
+      update_at_year: year,
+      update_at_date: date
+    };
+    postList.push(obj);
+  });
+  return postList;
+};
+const getYearByPostList = posts => {
+  const years = [];
+  posts.forEach(post => {
+    years.push(+post.update_at_year);
+  });
+  const notRepeatYears = [...new Set(years)];
+  const yearSortToDesc = notRepeatYears.sort((a, b) => b - a); // 降序
+  const contentToString = yearSortToDesc.map(year => String(year));
+  return contentToString;
+};
+const getYearListByPostList = (years, posts) => {
+  const yearList = [];
+  years.forEach(year => {
+    let obj = { year: '', list: [] };
+    obj.year = String(year);
+    posts.forEach(post => {
+      if (year === post.update_at_year) {
+        obj.list.push(post);
+      }
+    });
+    yearList.push(obj);
+  });
+  return yearList;
+};
+
+export default defineEventHandler(async event => {
+  const query = getQuery(event);
+
+  try {
+    const postList = await getPostListFromFirebase();
+    const years = getYearByPostList(postList);
+    const yearList = getYearListByPostList(years, postList);
+
+    const currentYear = query.year ? query.year : years[0];
+    const filterCurrentYearOfList = yearList.filter(item => item.year === currentYear);
+
+    return {
+      status: 'success',
+      data: {
+        years,
+        postList: filterCurrentYearOfList
+      }
+    };
+  } catch (error) {
+    throw createError({ statusCode: 500, statusMessage: '取得失敗，請稍候' });
+  }
+});
