53f7197506763f18f6537c26fa8e27940f24a1ca
Mandy Tsai <mandy3152618@gmail.com>
2023-02-08 09:51:35 +0800

    fix: /api/posts/memberId/list 文章管理 api

--- server/api/firebase/posts/[memberId]/list.get.js
+++ server/api/firebase/posts/[memberId]/list.get.js
@@ -2,10 +2,28 @@ import { markdownToTxt } from '@/server/utils/useMarkdown';
 import firebaseServer from '@/server/utils/useFirebaseServer';
 import pagination from '@/server/utils/usePagination';
 
-const sortListMap = new Map([
-  [0, 'desc'],
-  [1, 'asc']
-]);
+const getPostListFromFirebase = async ({ currentState, currentSort, memberId }) => {
+  const { db } = firebaseServer();
+  const postsRef = db.collection('posts');
+
+  const sortListMap = new Map([
+    [0, 'desc'],
+    [1, 'asc']
+  ]);
+
+  const snapshot = await postsRef
+    .where('status', '==', currentState)
+    .where('member_id', '==', memberId)
+    .orderBy('update_time', sortListMap.get(currentSort))
+    .get();
+
+  const postList = [];
+  snapshot.forEach(doc => {
+    postList.push(doc.data());
+  });
+
+  return postList;
+};
 
 const getSearchFilterData = (data, currentSearch) => {
   const strArray = currentSearch.split(' ');
@@ -23,34 +41,26 @@ const getSearchFilterData = (data, currentSearch) => {
 };
 
 export default defineEventHandler(async event => {
+  const urlQuery = getQuery(event);
+  const params = getRouterParams(event);
+
+  const currentState = urlQuery.publishState === 'On' ? 'public' : 'draft'; // 狀態
+  const currentPage = +urlQuery.page || 1; // 頁數
+  const currentSort = +urlQuery.sort || 0; // 排序
+  const currentSearch = urlQuery.search || ''; // 搜尋
+  const memberId = params.memberId || ''; // 會員 id
   try {
-    const urlQuery = getQuery(event);
-    const currentState = urlQuery.publishState === 'On' ? 'public' : 'draft';
-    const currentPage = +urlQuery.page;
-    const currentSort = +urlQuery.sort || 0; // 預設排序: 時間新->舊
-    const currentSearch = urlQuery.search || '';
-
-    // firebase
-    const params = getRouterParams(event);
-    const { db } = firebaseServer();
-    const postsRef = db.collection('posts');
-
-    const snapshot = await postsRef
-      .where('status', '==', currentState)
-      .where('member_id', '==', params.memberId)
-      .orderBy('update_time', sortListMap.get(currentSort))
-      .get();
-    let data = [];
-    snapshot.forEach(doc => {
-      data.push(doc.data());
+    let postList = await getPostListFromFirebase({
+      currentState,
+      currentSort,
+      memberId
     });
-    console.log('data', data);
 
     if (currentSearch !== '') {
-      data = getSearchFilterData(data, currentSearch);
+      postList = getSearchFilterData(postList, currentSearch);
     }
 
-    const result = pagination({ currPage: currentPage, perPage: 2, articles: data });
+    const result = pagination({ currPage: currentPage, perPage: 2, articles: postList });
 
     return {
       status: 'success',
