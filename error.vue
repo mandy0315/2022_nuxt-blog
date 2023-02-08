0ec3cbe1b299ba0156a8f1bfa10770e1653f6925
Mandy Tsai <mandy3152618@gmail.com>
2023-02-08 13:56:40 +0800

    fix: 404 頁面按鈕返回頁面的判斷

--- error.vue
+++ error.vue
@@ -2,7 +2,9 @@
   <nuxt-layout :name="isDashboardPages ? 'dashboard' : 'default'">
     <div v-if="error.statusCode === '404'" class="c-container flex flex-col items-center">
       <h1 class="pt-20 pb-8 text-center text-4xl font-bold">404 找不到此頁面</h1>
-      <nuxt-link :to="isDashboardPages ? '/dashboard' : '/'" class="c-text-button">返回頁面</nuxt-link>
+      <nuxt-link :to="isDashboardPages ? '/dashboard' : '/'" class="c-text-button">{{
+        isDashboardPages ? '回筆記管理頁' : '回首頁'
+      }}</nuxt-link>
     </div>
   </nuxt-layout>
 </template>
