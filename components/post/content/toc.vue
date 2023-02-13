53e3f8fbe3d8f6260c22118ad6ccc13fdc896951
Mandy Tsai <mandy3152618@gmail.com>
2023-02-13 18:11:11 +0800

    fix: 沒有目錄判斷與整理 code

--- components/post/content/toc.vue
+++ components/post/content/toc.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav v-if="tocList.length !== 0" class="relative pl-14" :class="hiddenToc ? 'w-0' : 'w-60'">
+  <nav class="relative pl-14" :class="hiddenToc ? 'w-0' : 'w-60'">
     <div :class="isScrollFixed ? 'fixed top-16' : 'absolute top-0'">
       <button @click="hiddenToc = !hiddenToc" class="absolute right-full border p-2 text-lg">
         <Icon v-if="hiddenToc" icon="material-symbols:keyboard-double-arrow-left-rounded" />
@@ -7,7 +7,7 @@
       </button>
       <div v-if="!hiddenToc" class="px-4 py-2">
         <p class="pb-1 text-sm text-c-gray-400">目錄</p>
-        <div v-if="tocList.length > 0" class="">
+        <template v-if="tocList.length > 0">
           <div
             v-for="anchor in tocList"
             :key="anchor.title"
@@ -18,7 +18,8 @@
           >
             <a :href="`#${anchor.id}`" class="cursor-pointer">{{ anchor.title }}</a>
           </div>
-        </div>
+        </template>
+        <div v-else>目前沒有目錄</div>
       </div>
     </div>
   </nav>
@@ -30,10 +31,13 @@ import { useMainStore } from '@/stores/index';
 const $mainStore = useMainStore();
 const $route = useRoute();
 const { scrollToSection } = useScrollTo();
-
 const mdPreview_el = useState('mdPreview_el');
+
 const currentSection = ref('');
 
+const headerBottomHeight = computed(() => $mainStore.els.headerBottom_el?.offsetHeight || 0);
+
+// 目錄展開縮小
 const hiddenToc = ref(false);
 watch(hiddenToc, isHidden => {
   if (isHidden) {
@@ -43,12 +47,9 @@ watch(hiddenToc, isHidden => {
   }
 });
 
-const headerBottomHeight = computed(() => $mainStore.els.headerBottom_el?.offsetHeight || 0);
-
 const tocList = ref([]);
 const getTocList = () => {
-  tocList.value = [];
-  let data = [];
+  let tocList = [];
 
   const anchors = mdPreview_el.value.$el.querySelectorAll('h2,h3,h4,h5,h6');
   let anchorsToArr = [...anchors].filter(anchor => !!anchor.innerText.trim());
@@ -58,14 +59,22 @@ const getTocList = () => {
 
   const hTags = [...new Set(anchorsToArr.map(anchor => anchor.tagName).sort())];
 
-  data = anchorsToArr.map(anchor => ({
+  tocList = anchorsToArr.map(anchor => ({
     id: anchor.id,
     title: anchor.innerText,
     indent: hTags.indexOf(anchor.tagName)
   }));
-  tocList.value = data;
+  return tocList;
 };
 
+// css 設定 scroll-padding-top
+const setScrollPaddingStyle = () => {
+  document.querySelectorAll('html,body').forEach(el => {
+    el.style.cssText = `scroll-padding-top: ${headerBottomHeight.value + 10}px;`;
+  });
+};
+
+// 網址有錨點滾到區塊
 const URLHashScrollToSection = () => {
   const { hash } = $route;
 
@@ -83,19 +92,12 @@ const URLHashScrollToSection = () => {
   }
 };
 
+// 監聽滾動事件
 const isScrollFixed = ref(false);
 const setTocFixed = () => {
   const topToMdPreviewHeight = mdPreview_el.value.$el.offsetTop;
   isScrollFixed.value = window.scrollY > topToMdPreviewHeight - headerBottomHeight.value;
 };
-
-const setScrollPaddingStyle = () => {
-  document.querySelectorAll('html,body').forEach(el => {
-    el.style.cssText = `scroll-padding-top: ${headerBottomHeight.value + 10}px;`;
-  });
-};
-
-// 偵測目前段落
 const setCurrentSection = () => {
   for (const anchor of tocList.value) {
     const hTag_el = mdPreview_el.value.$el.querySelector(`#${anchor.id}`);
@@ -104,7 +106,6 @@ const setCurrentSection = () => {
     }
   }
 };
-
 const handleScroll = () => {
   setTocFixed();
   setCurrentSection();
@@ -112,11 +113,16 @@ const handleScroll = () => {
 
 onMounted(() => {
   hiddenToc.value = !!localStorage.getItem('hiddenToc');
-  getTocList();
-  setScrollPaddingStyle();
-  URLHashScrollToSection();
-  currentSection.value = tocList.value[0].title;
-  window.addEventListener('scroll', handleScroll);
+  const data = getTocList();
+  if (data) {
+    tocList.value = data;
+    currentSection.value = tocList.value[0].title;
+
+    setScrollPaddingStyle();
+    URLHashScrollToSection();
+
+    window.addEventListener('scroll', handleScroll);
+  }
 });
 onUnmounted(() => {
   window.removeEventListener('scroll', handleScroll);
