31198eaa7f0e8703577c4f8c4bf28f3dd64f5262
Mandy Tsai <mandy3152618@gmail.com>
2023-02-21 17:17:10 +0800

    add: 客戶端渲染元件

--- components/aside/default.vue
+++ components/aside/default.vue
@@ -3,27 +3,29 @@
     <div>
       <p class="text-lg">關於我</p>
     </div>
-    <div>
-      <p class="text-lg">最新筆記 <span class="text-sm">( 顯示最新五筆 )</span></p>
-      <ul v-if="postList.length > 0">
-        <li v-for="post in postList" :key="post.id">
-          <nuxt-link :to="`/post/${post.id}`">
-            {{ post.title }}
-          </nuxt-link>
-        </li>
-      </ul>
-    </div>
-    <div>
-      <p class="text-lg">標籤</p>
-      <ul v-if="tagList.length > 0">
-        <li v-for="tag in tagList" :key="tag.name" class="inline-block">
-          <nuxt-link :to="`/tag/${tag.name}`">
-            <theTag :name="tag.name" :number="tag.count" />
-          </nuxt-link>
-        </li>
-      </ul>
-      <div v-else>目前沒有標籤喔～</div>
-    </div>
+    <client-only>
+      <div>
+        <p class="text-lg">最新筆記 <span class="text-sm">( 顯示最新五筆 )</span></p>
+        <ul v-if="postList.length > 0">
+          <li v-for="post in postList" :key="post.id">
+            <nuxt-link :to="`/post/${post.id}`">
+              {{ post.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p class="text-lg">標籤</p>
+        <ul v-if="tagList.length > 0">
+          <li v-for="tag in tagList" :key="tag.name" class="inline-block">
+            <nuxt-link :to="`/tag/${tag.name}`">
+              <theTag :name="tag.name" :number="tag.count" />
+            </nuxt-link>
+          </li>
+        </ul>
+        <div v-else>目前沒有標籤喔～</div>
+      </div>
+    </client-only>
   </div>
 </template>
 
@@ -33,7 +35,7 @@ const $asideStore = useAsideStore();
 const postList = computed(() => $asideStore.postList);
 const tagList = computed(() => $asideStore.tagList);
 
-(async () => {
+(() => {
   if (postList.value.length === 0) {
     $asideStore.updateAllList();
   }
