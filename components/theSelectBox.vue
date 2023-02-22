2886674706520434402e39607522fbea35f6d322
Mandy Tsai <mandy3152618@gmail.com>
2023-02-22 11:18:58 +0800

    style: border 顏色更動

--- components/theSelectBox.vue
+++ components/theSelectBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     ref="container_el"
-    class="relative z-10 cursor-pointer rounded border border-solid border-c-gray-200 py-1 pl-2 text-c-gray-200"
+    class="relative z-10 cursor-pointer rounded border border-solid border-c-line-gray py-1 pl-2 text-c-gray-200"
     @click="toggleList"
   >
     <slot name="title"></slot>
@@ -10,7 +10,7 @@
       <Icon v-else icon="material-symbols:keyboard-arrow-down" class="inline-block text-xl" />
     </i>
 
-    <ul v-if="isOpen" class="absolute top-full right-0 mt-1 w-[120px] rounded border border-c-gray-200 bg-white py-1">
+    <ul v-if="isOpen" class="absolute top-full right-0 mt-1 w-[120px] rounded border border-c-line-gray bg-white py-1">
       <li
         v-for="i in selectList"
         :key="i"
