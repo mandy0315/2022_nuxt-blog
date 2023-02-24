5793d24b7610f1db9ebdffcfcdfa2ccb5cbe22a1
Mandy Tsai <mandy3152618@gmail.com>
2023-02-24 16:02:52 +0800

    add: theTooltip 元件

--- components/header/default.vue
+++ components/header/default.vue
@@ -2,30 +2,55 @@
   <header ref="headerRef">
     <!-- web -->
     <div
-      class="flex w-full items-center bg-c-bg-light py-2 px-4 dark:bg-c-bg-dark"
+      class="flex w-full items-center bg-c-bg-light py-1 px-4 dark:bg-c-bg-dark"
       :class="{ 'an-slide-down fixed top-0 z-10 shadow': isFixHeader }"
     >
-      <nuxt-link to="/" class="mr-10 w-40 overflow-hidden">
+      <NuxtLink to="/" class="mr-10 w-40 overflow-hidden">
         <img src="/static/images/logo.svg" :alt="config.public.WEBSITE_TITLE" />
-      </nuxt-link>
+      </NuxtLink>
       <nav class="font-bold">
-        <nuxt-link
-          v-for="item in menuList"
+        <NuxtLink
+          v-for="item in menuList['main']"
           :to="item.link"
           :key="item.link"
           class="relative mx-3 text-base hover:text-c-brown-200"
           :class="{
-            'text-c-brown-200 after:absolute after:left-0 after:-bottom-4 after:inline-block after:h-1 after:w-full after:bg-c-brown-200 after:content-[attr(after)] ':
+            'text-c-brown-200 after:absolute after:-bottom-4 after:left-0 after:inline-block after:h-1 after:w-full after:bg-c-brown-200 after:content-[attr(after)] ':
               currentPath === item.link
           }"
         >
           {{ item.name }}
-        </nuxt-link>
+        </NuxtLink>
       </nav>
 
       <nav class="ju ml-auto flex items-center">
         <theColorMode class="mr-4" />
-        <nuxt-link to="/dashboard">後台</nuxt-link>
+        <div
+          ref="toggleRef"
+          class="relative flex h-8 cursor-pointer items-center justify-center rounded-full border border-solid border-c-line-gray bg-white px-3 text-c-black-100 transition-all duration-300 ease-in-out dark:bg-c-black-200"
+          @click="toggleList"
+        >
+          <Icon icon="fluent:calendar-settings-32-regular" class="text-2xl" />
+          <i class="pointer-events-none align-middle text-base">
+            <Icon v-if="isOpen" icon="material-symbols:keyboard-arrow-up" />
+            <Icon v-else icon="material-symbols:keyboard-arrow-down" />
+          </i>
+          <div v-if="isOpen" class="absolute top-full right-0 mt-4">
+            <theTooltip arrow="right">
+              <p class="border-b border-c-line-gray px-2 py-1 text-c-black-200">後台管理</p>
+              <div class="p-1">
+                <NuxtLink
+                  v-for="item in menuList['dashboard']"
+                  :to="item.link"
+                  :key="item.link"
+                  class="block rounded-md p-1 text-center text-c-gray-200 hover:bg-c-gray-100/80 hover:text-c-black-200"
+                  to="/dashboard/post/edit"
+                  >{{ item.name }}</NuxtLink
+                >
+              </div>
+            </theTooltip>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
@@ -34,16 +59,30 @@
 <script setup>
 import { useMainStore } from '@/stores/index';
 
-const menuList = [
-  {
-    link: '/',
-    name: '首頁'
-  },
-  {
-    link: '/archive',
-    name: '所有筆記'
-  }
-];
+const menuList = {
+  main: [
+    {
+      link: '/',
+      name: '首頁'
+    },
+    {
+      link: '/archive',
+      name: '所有筆記'
+    }
+  ],
+  dashboard: [
+    {
+      link: '/dashboard/post/manage-public',
+      name: '筆記管理'
+    },
+    {
+      link: '/dashboard/post/edit',
+      name: '新增筆記'
+    }
+  ]
+};
+
+//
 
 const $mainStore = useMainStore();
 const config = useRuntimeConfig();
@@ -60,6 +99,12 @@ onMounted(() => {
   window.addEventListener('scroll', handleScrollShow);
 });
 onUnmounted(() => window.removeEventListener('scroll', handleScrollShow));
+
+const { toggleList, isOpen, setContainer } = useToggle();
+const toggleRef = ref(null);
+onMounted(() => {
+  setContainer(toggleRef.value);
+});
 </script>
 
 <style lang="scss" scoped>
---
+++ components/theTooltip.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  arrow: {
+    type: String,
+    default: 'center'
+  }
+});
+const arrowPosStyle = computed(() => `arrow-${props.arrow}`);
+</script>
+
+<template>
+  <div class="relative min-h-[2rem] min-w-[9.25rem] rounded-md bg-white shadow-select-box">
+    <div class="arrow" :class="arrowPosStyle"></div>
+    <slot />
+  </div>
+</template>
+<style lang="scss" scoped>
+.arrow {
+  @apply absolute -top-2 h-2 w-4 overflow-hidden;
+  &:before {
+    @apply absolute top-1 block h-4 w-4 rotate-45  bg-white shadow-select-box content-[''];
+  }
+  &-left {
+    @apply left-2;
+  }
+  &-right {
+    @apply right-2;
+  }
+  &-center {
+    @apply left-[calc(50%-0.5rem)];
+  }
+}
+</style>
